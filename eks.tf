c914c2da915fe10ec8f59b5a5dfd1eeea54d469e
rafael.ferreira <rafael.ferreira@softplan.com.br>
2024-10-01 20:56:52 -0300

    Criação do node group no eks

--- eks.tf
+++ eks.tf
@@ -1,3 +1,39 @@
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = [
+            "ec2.amazonaws.com",
+            "eks.amazonaws.com"
+          ]
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
 resource "aws_eks_cluster" "foodway_eks_cluster" {
   name     = "foodway_eks_cluster_j5431"
   role_arn = var.lab_role
@@ -12,7 +48,7 @@ resource "aws_eks_cluster" "foodway_eks_cluster" {
 resource "aws_eks_node_group" "foodway_eks_node_group" {
   cluster_name    = aws_eks_cluster.foodway_eks_cluster.name
   node_group_name = "foodway_eks_node"
-  node_role_arn   = var.lab_role
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = aws_subnet.private_subnets[*].id
 
   instance_types = ["t3.medium"]
